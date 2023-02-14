d091a26e96800b7349dd3f823510ba9ae03d940a
Qudusayo <qqudusayo@gmail.com>
2023-02-14 14:30:13 +0100

    fix: api route

--- src/pages/JobTracking/JobTracking.jsx
+++ src/pages/JobTracking/JobTracking.jsx
@@ -15,16 +15,61 @@ const Section = styled.section`
   flex-direction: column;
 `;
 const JobTracking = () => {
+  const [formEntry, setFormEntry] = React.useState({
+    fullname: "",
+    applicationId: "",
+  });
+
+  const inputHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setFormEntry((formEntry) => ({
+      ...formEntry,
+      [name]: value,
+    }));
+  };
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(formEntry);
+
+    // FIX THE API ENDPOINT HERE
+    let res = await axios.post("/api", formEntry);
+    let { status } = res.data;
+
+    if (status.toLowerCase() === "accepted") {
+      return Swal.fire("Whao", "Your application is accepted", "success");
+    }
+
+    if (status.toLowerCase() === "pending") {
+      return Swal.fire("Hello", "Your submission is pending", "warning");
+    }
+
+    if (status.toLowerCase() === "rejected") {
+      return Swal.fire("Oops", "Your application is rejected", "error");
+    }
+  };
   return (
     <Layout>
       <Section>
         <h2 className={styles.Title}>Job Tracking Form</h2>
-        <form className={styles.Form}>
+        <form className={styles.Form} onSubmit={submitHandler}>
           <div>
-            <InputComponent label={"Full Name"} />
+            <InputComponent
+              value={formEntry.fullname}
+              changeHandler={inputHandler}
+              name="fullname"
+              label={"Full Name"}
+            />
           </div>
           <div>
-            <InputComponent label={"Application ID"} />
+            <InputComponent
+              value={formEntry.applicationId}
+              changeHandler={inputHandler}
+              name="applicationId"
+              label={"Application ID"}
+            />
           </div>
           <ButtonComponent
             label="Submit"
